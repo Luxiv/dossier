887d4b8a79a61d841bb23ffe7e3e49082217e3c4
Sribniak Taras <sribniak.terry@protonmail.com>
2023-02-08 15:15:23 +0200

    Added DocComments

--- app.py
+++ app.py
@@ -2,33 +2,37 @@ from flask import render_template, request
 from models import *
 
 
-@app.route('/dossier/', methods=['GET'])
+@app.route('/dossier/', methods=['GET'])  # оприділяємо url по якій буде відкриватися додаток
 def patient_dossier():
-    if request.args.get('patient_id'):
-        patient_id = request.args.get('patient_id')
-        patient_id = int(patient_id)
-        patient_general_info = PatientDossier.query.filter(PatientDossier.id == patient_id).one()
+    if request.args.get('patient_id'):  # в юрл передається аргумент patient_id що визначає на сторінку якого пацієнта ти попадаєш
+        patient_id = request.args.get('patient_id')  # тут ми хапаємо patient_id з урли виглядає так: patient_id=1
+        patient_id = int(patient_id)  #  переводимо його у потрібний тип данних
+        patient_general_info = PatientDossier.query.filter(PatientDossier.id == patient_id).one()  # витягуємо з БД всі дані об"єкта PatientDossier фільтруючи всіх пацієнтів по його айді
 
-        if request.args.get('info') == 'anamnesis':
-            patient_anamnesis_info = PatientAnamnesis.query.filter(PatientAnamnesis.id == patient_id).one()
-            complaints = PostComplaints.query.filter(PostComplaints.patient_id == patient_id)
-            return render_template('anamnez.html',
-                                   patient=patient_general_info,
+        if request.args.get('info') == 'anamnesis':  #  аргумент info=anamnesis в урлі перекидає на вкладку анамнез
+            patient_anamnesis_info = PatientAnamnesis.query.filter(PatientAnamnesis.id == patient_id).one()  #  аналогічним чином фільтруємо пацієнтів по айдішці і витягуємо всі данні обєкта анамнез для цього пацієнта
+            #  complaints - скарги
+            complaints = PostComplaints.query.filter(PostComplaints.patient_id == patient_id)  # оскільки в об"єкта пацієнт, може бути багато скарг фільтруємо скарги по стовпцю patient_id
+            return render_template('anamnez.html',  #  повертаємо рендер anamnez.html де дані пацієнта вже передані в потрібні поля
+                                   patient=patient_general_info,  # ці три строчки передають в anamnez.html динамічні дані з БД
                                    anamnesis=patient_anamnesis_info,
                                    complaints=complaints)
 
-        elif request.args.get('info') == 'general':
+        elif request.args.get('info') == 'general':  #  аргумент info=general в урлі перекидає на вкладку Общеее
             imt = (patient_general_info.weight / patient_general_info.height ** 2)*10**4 if patient_general_info.weight is not None or patient_general_info.height is not None else None
-            return render_template('index.html',
+            # тут рахуємо імт (поки розрахунок не бере до уваги стать, поправимо це коли будемо відшліфовути додаток )
+            return render_template('index.html',  #  повертаємо рендер index.html з даними  з БД
                                    patient=patient_general_info,
                                    imt=f"{imt:.2f}")
-    else:
-        patients = PatientDossier.query.all()
-        return render_template('patientslist.html',
+    else:  # якщо в лінку не переданий жодний аргумент то повертається список пацієнтів з бд
+        patients = PatientDossier.query.all()  #  витягуємо всіх пацієнтів з бд
+        return render_template('patientslist.html',  # тут всьо і так понятно)
                                patients=patients)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # якщо ми запустимо поточний файл через IDE або через термінал, наступним чином:
+    #  1) перейти в дерикторію з поточним файлом  2) запустити команду: python app.py
+    # запуститься локальний сервер де можна поцикати додаток, щоб це зробити треба перейти по урлі знизу
     app.run(debug=True)
 
 # http://127.0.0.1:5000/dossier/?info=general&patient_id=1
--- models.py
+++ models.py
@@ -2,6 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 from flask import Flask
 
+
+#  Нижче ініціалізуємо додаток і оприділяємо конфігурацію БД (зараз ми використовуємо бд sqlite, тут може бути конфіг для постгрес і такого іншого)
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dossier.db'
@@ -9,8 +11,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 
-class PatientDossier(db.Model):
-    __tablename__      = 'patient_general'
+class PatientDossier(db.Model):  # творюємо обєкт PatientDossier з даними що ми бачимо в вкладці Общее
+    __tablename__      = 'patient_general'  # внутрішня назва таблиці в бд
+    # оприділяємо всі стовпці з даними по обєкту PatientDossier(тіпа всі дані зі сторінки Общее)
     id                 = db.Column(db.Integer(), primary_key=True)
     name_surname       = db.Column(db.String(100), unique=True)
     price              = db.Column(db.Integer())
@@ -27,28 +30,30 @@ class PatientDossier(db.Model):
     cancellations      = db.Column(db.Integer())
     desired_frequency  = db.Column(db.Integer())
 
-    def __init__(self, name, phone_number):
+    def __init__(self, name, phone_number):  #  це теж внутрішнє тобі не треба. Коли в коді викликається обєкт PatientDossier без визначення конкретних полів то повертається ім"я і телефон пацієнта по якому був запит
         self.name = name
         self.phone_number = phone_number
 
 
-class PatientAnamnesis(db.Model):
+class PatientAnamnesis(db.Model):  # класс анамнез де описуються данні анамнезу
     __tablename__        = 'patient_anamnesis'
+    # напряму зв"яуємо айдішнік классу PatientAnamnesis і PatientDossier
     id                   = db.Column(db.Integer, ForeignKey('patient_general.id'), primary_key=True)
-    procedure_features   = db.Column(db.String(300))
-    diagnosis            = db.Column(db.String(300))
-    comorbidities        = db.Column(db.String(300))
-    pre_complaints       = db.Column(db.String(300))
+    procedure_features   = db.Column(db.String(300))  #  Особенности проведения процедуры
+    diagnosis            = db.Column(db.String(300))  #  Диагноз
+    comorbidities        = db.Column(db.String(300))  #  Сопутствующие заболевания
+    pre_complaints       = db.Column(db.String(300))  #  Жалобы до першого сеансу
 
 
-class PostComplaints(db.Model):
+class PostComplaints(db.Model):  # Скарги що виникли протягом терапії назвемо їх постскарги/постжалобы
     __tablename__        = 'complaints'
-    id                   = db.Column(db.Integer, primary_key=True)
+    id                   = db.Column(db.Integer, primary_key=True)  # у кожної постскарги буде унікальний айді
+    #  оскільки в пацієнта може бути багато постскарг (а може і не бути зовсім) прив"язуємо кожну з них до конкретного пацієнта полем patient_id
     patient_id           = db.Column(db.Integer, db.ForeignKey('patient_general.id'), nullable=False)
-    post_complaints      = db.Column(db.String(300))
-    post_complaint_date  = db.Column(db.DateTime)
+    post_complaints      = db.Column(db.String(300))  #  текст постскарги
+    post_complaint_date  = db.Column(db.DateTime)  #  дата постскарги
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # Наступні 3 строчки потрібні щоб згенерувати базуданих по правилах що передані вище!
     with app.app_context():
         db.create_all()
